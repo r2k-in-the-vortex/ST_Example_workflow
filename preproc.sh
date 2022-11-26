6035ddfec4e96275c52e74d9ecc5e2d18ba72208
r2k <r2k@asd.asd>
2022-11-26 11:08:58 +0200

    also compile, assumes no problems with matiec though...

--- preproc.sh
+++ preproc.sh
@@ -5,3 +5,8 @@ do
 inc+=" -I $dirname"
 done
 gpp src/Config0.ST $inc -o build/result.ST
+
+cd build/
+iec2c -r -R -I ~/matiec/lib/ result.ST
+g++ -I ~/matiec/lib/C/ -c Config0.c
+g++ -I ~/matiec/lib/C/ -c Res0.c
